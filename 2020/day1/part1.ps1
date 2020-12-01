49eaf679dd4abf645695155aaa6244cb6465b06b
William Johansson <William.Johansson@pwc.com>
2020-12-01 13:41:09 +0100

    2020 day 1

---
+++ 2020/day1/part1.ps1
@@ -0,0 +1,15 @@
+﻿# Ref: https://adventofcode.com/2020/day/1
+
+$PuzzleInput = Get-Content '.\input.ps1'
+
+foreach ($a in $PuzzleInput) {
+
+    for ($i=0; $i -lt $PuzzleInput.Count; $i++) {
+
+        if ([int]$a + [int]$PuzzleInput[$i] -eq 2020) {
+            Write-Host ([int]$a * [int]$PuzzleInput[$i])
+            exit
+        }
+        #Write-Host $item
+    }
+}
---
+++ 2020/day1/part2.ps1
@@ -0,0 +1,18 @@
+﻿# Ref: https://adventofcode.com/2020/day/1
+
+$PuzzleInput = Get-Content '.\input.ps1'
+
+foreach ($a in $PuzzleInput) {
+
+    for ($i=0; $i -lt $PuzzleInput.Count; $i++) {
+
+        for ($ii=0; $ii -lt $PuzzleInput.Count; $ii++) {
+
+            if ([int]$a + [int]$PuzzleInput[$i] +[int]$PuzzleInput[$ii] -eq 2020) {
+                Write-Host ([int]$a * [int]$PuzzleInput[$i] * [int]$PuzzleInput[$ii] )
+                exit
+            }
+        }
+
+    }
+}
