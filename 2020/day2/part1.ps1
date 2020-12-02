db3bab30e1f199a61f568db803fa41ae605c3ade
William Johansson <William.Johansson@pwc.com>
2020-12-02 09:47:19 +0100

    Day 2 part 1

---
+++ 2020/day2/part1.ps1
@@ -0,0 +1,25 @@
+﻿<#
+    --- Day 2: Password Philosophy ---
+    https://adventofcode.com/2020/day/2
+#>
+
+$PuzzleInput = Get-Content '.\input.txt'
+$i = 0
+
+foreach ($line in $PuzzleInput) {
+
+    $inputString = ([regex]::Matches($line,'([a-zA-Z]*)$')).Value
+    
+    $PasswordPolicy_Count = ([regex]::matches($line, '^\d*-\d*')).Value
+    $PasswordPolicy_Char = (([regex]::matches($line,'[a-zA-Z]:')).Value).Replace(':','')
+    $PasswordPolicy_CountMin = $PasswordPolicy_Count.Split('-')[0]
+    $PasswordPolicy_CountMax = $PasswordPolicy_Count.Split('-')[1]
+
+    $CharMatches = ([regex]::Matches($inputString, $PasswordPolicy_Char)).count
+
+    if ( $CharMatches -ge $PasswordPolicy_CountMin -and $CharMatches -le $PasswordPolicy_CountMax ) {
+        $i++
+    }
+}
+
+return $i
